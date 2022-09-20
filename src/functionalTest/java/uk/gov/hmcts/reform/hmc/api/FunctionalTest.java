811f3f56272f7b7614ba9160802bcdf4110ae4ab
Mohammed Murshad Haque <murshad.haque@HMCTS.NET>
2022-09-20 14:27:53 +0100

    ISDB-374: Smoke test source include Functional test

---
+++ src/functionalTest/java/uk/gov/hmcts/reform/hmc/api/FunctionalTest.java
@@ -0,0 +1,37 @@
+package uk.gov.hmcts.reform.hmc.api;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FunctionalTest {
+
+    private final String targetInstance =
+        StringUtils.defaultIfBlank(
+            System.getenv("TEST_URL"),
+            "http://localhost:4550"
+        );
+
+
+    @Test
+    void shouldCheckApplicationHealthUp() {
+
+        RequestSpecification requestSpecification = new RequestSpecBuilder()
+            .setBaseUri(targetInstance)
+            .setRelaxedHTTPSValidation()
+            .build();
+
+        Response response = given(requestSpecification)
+            .when()
+            .get("/health")
+            .then()
+            .extract().response();
+
+        assertThat(response.getBody().asString()).contains("UP");
+    }
+}
--- src/smokeTest/java/uk/gov/hmcts/reform/hmc/api/SmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/hmc/api/SmokeTest.java
@@ -1,4 +1,4 @@
-package uk.gov.hmcts.reform.controllers;
+package uk.gov.hmcts.reform.hmc.api;
 
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
