406711066874683a5ce8ba1f8de171fdcf5f91f2
“jegan-maharajan1” <maharajan.jegan1@hmcts.net>
2022-10-23 00:16:46 +0100

    preview fix

--- src/main/java/uk/gov/hmcts/reform/hmc/api/config/ServiceBus.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/api/config/ServiceBus.java
@@ -1,5 +1,9 @@
 package uk.gov.hmcts.reform.hmc.api.config;
 
+import com.azure.messaging.servicebus.ServiceBusClientBuilder;
+import com.azure.messaging.servicebus.ServiceBusReceiverAsyncClient;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicBoolean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,5 +16,16 @@ public class ServiceBus {
         String connectionString = "dummy";
         String topicName = "hmc-to-cft-aat";
         String subscriptionName = "hmc-subs-to-cft-aat";
+
+        AtomicBoolean sampleSuccessful = new AtomicBoolean(true);
+        CountDownLatch countdownLatch = new CountDownLatch(1);
+        ServiceBusReceiverAsyncClient receiver =
+                new ServiceBusClientBuilder()
+                        .connectionString(connectionString)
+                        .receiver()
+                        .disableAutoComplete()
+                        .topicName(topicName)
+                        .subscriptionName(subscriptionName)
+                        .buildAsyncClient();
     }
 }
