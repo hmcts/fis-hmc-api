60eb13ef74781c308bc5a0f103e25f2a7088d843
“jegan-maharajan1” <maharajan.jegan1@hmcts.net>
2022-10-23 00:05:52 +0100

    preview fix

--- src/main/java/uk/gov/hmcts/reform/hmc/api/config/ServiceBus.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/api/config/ServiceBus.java
@@ -1,11 +1,16 @@
 package uk.gov.hmcts.reform.hmc.api.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ServiceBus {
 
-    public void test() {
+    @Bean
+    public void run() throws InterruptedException {
         System.out.println("test");
+        String connectionString = "dummy";
+        String topicName = "hmc-to-cft-aat";
+        String subscriptionName = "hmc-subs-to-cft-aat";
     }
 }
