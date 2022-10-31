96c323665de843cb8b17d87f1fe033e690cfa45e
hmcts-shajahan <mohamedshajahan.syedhussain@hmcts.net>
2022-10-31 14:41:22 +0000

    fix

--- src/main/java/uk/gov/hmcts/reform/hmc/api/controllers/HearingsController.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/api/controllers/HearingsController.java
@@ -1,6 +1,6 @@
 package uk.gov.hmcts.reform.hmc.api.controllers;
-import com.fasterxml.jackson.core.JsonProcessingException;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
@@ -8,7 +8,11 @@ import io.swagger.annotations.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.hmc.api.model.request.HearingsRequest;
 import uk.gov.hmcts.reform.hmc.api.model.response.Categories;
 import uk.gov.hmcts.reform.hmc.api.model.response.Hearings;
@@ -17,22 +21,19 @@ import uk.gov.hmcts.reform.hmc.api.services.HearingsService1;
 
 /** Hearings controller to get data hearings data. */
 @Slf4j
-@RequestMapping(path = "/" )
+@RequestMapping(path = "/")
 @RestController
-@Api(value = "/",description = "Standard API")
+@Api(value = "/", description = "Standard API")
 public class HearingsController {
     @Autowired private HearingsService hearingsService;
 
     @Autowired private HearingsService1 hearingsService1;
 
     @GetMapping(path = "/hearingsdata")
-    @ApiOperation(value = "get hearings data")
+    @ApiOperation("get hearings data")
     @ApiResponses(
             value = {
-                @ApiResponse(
-                        code = 200,
-                        message = "get hearings data successfully"),
-
+                @ApiResponse(code = 200, message = "get hearings data successfully"),
                 @ApiResponse(code = 400, message = "Bad Request")
             })
     public ResponseEntity<Categories> getHearingsData(
@@ -43,13 +44,10 @@ public class HearingsController {
     }
 
     @GetMapping(path = "/hearings")
-    @ApiOperation(value = "get hearings by case reference number")
+    @ApiOperation("get hearings by case reference number")
     @ApiResponses(
             value = {
-                @ApiResponse(
-                        code = 200,
-                        message = "get hearings by caseRefNo successfully"),
-
+                @ApiResponse(code = 200, message = "get hearings by caseRefNo successfully"),
                 @ApiResponse(code = 400, message = "Bad Request")
             })
     public Hearings getHearingsByCaseRefNo(
