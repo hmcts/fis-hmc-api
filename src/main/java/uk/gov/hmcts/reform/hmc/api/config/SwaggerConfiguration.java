870924e078823d34b5ebc97776150f41464495ab
hmcts-shajahan <mohamedshajahan.syedhussain@hmcts.net>
2022-10-31 19:09:09 +0000

    fix

---
+++ src/main/java/uk/gov/hmcts/reform/hmc/api/config/SwaggerConfiguration.java
@@ -0,0 +1,42 @@
+package uk.gov.hmcts.reform.hmc.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import uk.gov.hmcts.reform.hmc.api.Application;
+
+@Configuration
+// @EnableSwagger2
+public class SwaggerConfiguration implements WebMvcConfigurer {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+            .useDefaultResponseMessages(false)
+            .select()
+            .apis(
+                RequestHandlerSelectors.basePackage(
+                    Application.class.getPackage().getName() + ".endpoints"))
+            .paths(PathSelectors.any())
+            .build();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/swagger-ui/**")
+            .addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/")
+            .resourceChain(false);
+    }
+}
+
+
+
+
+
+
+
