38f802109135ceee971df120e4bbcaa92ed87499
hmcts-shajahan <mohamedshajahan.syedhussain@hmcts.net>
2022-11-15 19:37:33 +0000

    Resource Loader impl11

--- src/main/java/uk/gov/hmcts/reform/hmc/api/services/HearingsDataServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/api/services/HearingsDataServiceImpl.java
@@ -7,8 +7,6 @@ import static uk.gov.hmcts.reform.hmc.api.utils.Constants.FL401;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import lombok.RequiredArgsConstructor;
@@ -93,13 +91,6 @@ public class HearingsDataServiceImpl implements HearingsDataService {
         JSONObject screenFlowJson = null;
         JSONParser parser = new JSONParser();
         Resource resource = resourceLoader.getResource("classpath:ScreenFlow.json");
-
-        try (InputStream inputStream =
-                Files.newInputStream(Paths.get(resource.getFile().getPath()))) {
-            screenFlowJson = (JSONObject) parser.parse(new InputStreamReader(inputStream, "UTF-8"));
-        } catch (Exception e) {
-            log.error(e.getMessage());
-        }
 
         try (InputStream inputStream = resource.getInputStream()) {
             screenFlowJson = (JSONObject) parser.parse(new InputStreamReader(inputStream, "UTF-8"));
