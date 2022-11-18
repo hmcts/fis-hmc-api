26a61d122f3656d94fe6473b4d447013e600ba79
hmcts-shajahan <mohamedshajahan.syedhussain@hmcts.net>
2022-11-18 12:38:56 +0000

    funtional test case

---
+++ src/functionalTest/java/uk/gov/hmcts/reform/hmc/api/utils/IdamUserAuthTokenGenerator.java
@@ -0,0 +1,85 @@
+package uk.gov.hmcts.reform.hmc.api.utils;
+
+import static org.springframework.http.HttpHeaders.EMPTY;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+import uk.gov.hmcts.reform.hmc.api.exceptions.PrlUpdateException;
+import uk.gov.hmcts.reform.hmc.api.model.response.IdamTokenResponse;
+
+@TestPropertySource("classpath:application_e2e.yaml")
+@Service
+public class IdamUserAuthTokenGenerator {
+
+    @Value("${idam.serviceAccount.clientId}")
+    private String clientId;
+
+    @Value("${idam.serviceAccount.clientSecret}")
+    private String clientSecret;
+
+    @Value("${idam.serviceAccount.grantType}")
+    private String grantType;
+
+    @Value("${idam.serviceAccount.username}")
+    private String username;
+
+    @Value("${idam.serviceAccount.password}")
+    private String password;
+
+    @Value("${idam.serviceAccount.scope}")
+    private String scope;
+
+    RestTemplate restTemplateIdam = new RestTemplate();
+
+    @Value("${idam.url}")
+    private String idamBaseUrl;
+
+    private static final String TOKEN_ENDPOINT_PATH = "/o/token";
+
+    private static final Logger LOG = LoggerFactory.getLogger(IdamUserAuthTokenGenerator.class);
+
+    public String getSecurityTokens() {
+        LOG.info("getSecurityTokens");
+        LOG.info("clientId {}", clientId);
+        LOG.info("clientSecret {}", clientSecret);
+        if (null != idamBaseUrl) {
+            idamBaseUrl = idamBaseUrl.replace(".prod", "");
+        }
+        UriComponentsBuilder builder =
+                UriComponentsBuilder.newInstance()
+                        .fromUriString(idamBaseUrl + TOKEN_ENDPOINT_PATH)
+                        .queryParam("client_id", clientId)
+                        .queryParam("client_secret", clientSecret)
+                        .queryParam("grant_type", grantType)
+                        .queryParam("password", password)
+                        .queryParam("scope", scope)
+                        .queryParam("username", username);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+
+        try {
+            ResponseEntity<IdamTokenResponse> idamTokenResponse =
+                    restTemplateIdam.exchange(
+                            builder.build(false).toUriString(),
+                            HttpMethod.POST,
+                            new HttpEntity<>(httpHeaders, EMPTY),
+                            IdamTokenResponse.class);
+            return idamTokenResponse.getBody().getAccessToken();
+        } catch (HttpClientErrorException | HttpServerErrorException exception) {
+            LOG.error("exception {}", exception.getMessage());
+            throw new PrlUpdateException("IDAM", exception.getStatusCode(), exception);
+        }
+    }
+}
