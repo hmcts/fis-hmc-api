56425160344d6c62308d53a4e435923cac10daf4
hmcts-kodatala <purushotham.kodatala@hmcts.net>
2022-11-22 17:43:11 +0000

    updated response payload serviceValue api

--- src/main/java/uk/gov/hmcts/reform/hmc/api/model/response/IndividualDetailsModel.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/api/model/response/IndividualDetailsModel.java
@@ -1,7 +1,17 @@
 package uk.gov.hmcts.reform.hmc.api.model.response;
 
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@Builder(builderMethodName = "individualDetailsWith")
+@NoArgsConstructor
+@AllArgsConstructor
 public class IndividualDetailsModel {
     String title;
     String firstName;
