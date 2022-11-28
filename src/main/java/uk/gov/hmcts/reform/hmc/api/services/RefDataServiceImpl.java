6dfac766a0c54551752095f628a16ff20a0c0da0
hmcts-shajahan <mohamedshajahan.syedhussain@hmcts.net>
2022-11-28 19:31:32 +0000

    withoutFunc

--- src/main/java/uk/gov/hmcts/reform/hmc/api/services/RefDataServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/api/services/RefDataServiceImpl.java
@@ -3,8 +3,7 @@ package uk.gov.hmcts.reform.hmc.api.services;
 import feign.FeignException;
 import java.util.List;
 import java.util.stream.Collectors;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
@@ -17,6 +16,7 @@ import uk.gov.hmcts.reform.hmc.api.model.request.HearingUpdate;
 import uk.gov.hmcts.reform.hmc.api.model.response.CourtDetail;
 
 @Service
+@Slf4j
 @SuppressWarnings("unchecked")
 public class RefDataServiceImpl implements RefDataService {
 
@@ -24,7 +24,6 @@ public class RefDataServiceImpl implements RefDataService {
 
     @Autowired IdamTokenGenerator idamTokenGenerator;
     @Autowired RefDataApi refDataApi;
-    private static final Logger LOG = LoggerFactory.getLogger(RefDataServiceImpl.class);
 
     /**
      * This method will get all the court details of a particular venueId(epimmsId).
@@ -36,28 +35,28 @@ public class RefDataServiceImpl implements RefDataService {
     @SuppressWarnings("unused")
     public CourtDetail getCourtDetails(String epimmsId) {
         CourtDetail courtDetail = null;
-        LOG.info("calling getCourtDetails service " + epimmsId);
+        log.info("calling getCourtDetails service " + epimmsId);
         try {
             List<CourtDetail> courtDetailList =
                     refDataApi.getCourtDetails(
                             idamTokenGenerator.generateIdamTokenForRefData(),
                             authTokenGenerator.generate(),
                             epimmsId);
-            LOG.info("RefData call completed successfully" + courtDetailList);
+            log.info("RefData call completed successfully" + courtDetailList);
             List<CourtDetail> filteredCourtDetail =
                     courtDetailList.stream()
                             .filter(e -> "18".equals(e.getCourtTypeId()))
                             .collect(Collectors.toList());
             if (!filteredCourtDetail.isEmpty()) {
                 courtDetail = filteredCourtDetail.get(0);
-                LOG.info("Court details filtered" + courtDetail);
+                log.info("Court details filtered" + courtDetail);
             }
             return courtDetail;
         } catch (HttpClientErrorException | HttpServerErrorException exception) {
-            LOG.info("RefData call HttpClientError exception {}", exception.getMessage());
+            log.info("RefData call HttpClientError exception {}", exception.getMessage());
             throw new RefDataException("RefData", exception.getStatusCode(), exception);
         } catch (FeignException exception) {
-            LOG.info("RefData call Feign exception {}", exception.getMessage());
+            log.info("RefData call Feign exception {}", exception.getMessage());
         }
         return courtDetail;
     }
@@ -71,11 +70,11 @@ public class RefDataServiceImpl implements RefDataService {
     @Override
     @SuppressWarnings("unused")
     public Hearing getHearingWithCourtDetails(Hearing hearing) {
-        LOG.info(
+        log.info(
                 "calling getHearingWithCourtDetails service "
                         + hearing.getHearingUpdate().getHearingVenueId());
         CourtDetail courtDetail = getCourtDetails(hearing.getHearingUpdate().getHearingVenueId());
-        LOG.info("courtDetails " + courtDetail);
+        log.info("courtDetails " + courtDetail);
         if (courtDetail != null) {
             HearingUpdate hearingUpdate = hearing.getHearingUpdate();
             hearingUpdate.setHearingVenueName(courtDetail.getHearingVenueName());
