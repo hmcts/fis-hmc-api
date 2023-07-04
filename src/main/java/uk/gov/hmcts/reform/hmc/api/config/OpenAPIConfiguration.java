09241273b8bae08980d8e3e27ebabc266f942012
Yogendra Upasani <yogendra.upasani@hmcts.net>
2023-07-04 12:32:52 +0100

    Updated open API configuration

--- src/main/java/uk/gov/hmcts/reform/hmc/api/config/OpenAPIConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/api/config/OpenAPIConfiguration.java
@@ -1,5 +1,7 @@
 package uk.gov.hmcts.reform.hmc.api.config;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -8,6 +10,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@SecurityScheme(
+    name = "Bearer Authentication",
+    type = SecuritySchemeType.HTTP,
+    bearerFormat = "JWT",
+    scheme = "bearer"
+)
 public class OpenAPIConfiguration {
 
     @Bean
@@ -28,3 +36,4 @@ public class OpenAPIConfiguration {
                                 .url("https://github.com/hmcts/fis-hmc-api"));
     }
 }
+
