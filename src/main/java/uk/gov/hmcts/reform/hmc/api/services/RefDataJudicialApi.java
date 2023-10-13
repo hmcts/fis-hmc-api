42e07a5fc98ba2c8eb006262cef87ec062db14b7
Yogendra Upasani <yogendra.upasani@hmcts.net>
2023-10-13 12:40:45 +0100

    PRl-4421 - Migrate Judicial Ref data api to V2 version https://tools.hmcts.net/confluence/pages/viewpage.action?pageId=1682836961

--- src/main/java/uk/gov/hmcts/reform/hmc/api/services/RefDataJudicialApi.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/api/services/RefDataJudicialApi.java
@@ -1,8 +1,5 @@
 package uk.gov.hmcts.reform.hmc.api.services;
 
-import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
-
-import java.util.List;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -12,6 +9,8 @@ import uk.gov.hmcts.reform.hmc.api.config.RefDataConfiguration;
 import uk.gov.hmcts.reform.hmc.api.model.request.JudgeRequestDTO;
 import uk.gov.hmcts.reform.hmc.api.model.response.JudgeDetail;
 
+import java.util.List;
+
 @FeignClient(
     name = "ref-data-judicial-api",
     primary = false,
@@ -20,13 +19,17 @@ import uk.gov.hmcts.reform.hmc.api.model.response.JudgeDetail;
 public interface RefDataJudicialApi {
 
     String AUTHORIZATION = "Authorization";
+    String ACCEPT = "accept";
+    String CONTENT_TYPE_VAL = "application/json";
+
     String SERVICE_AUTHORIZATION = "ServiceAuthorization";
     String CONTENT_TYPE_V2 = "application/vnd.jrd.v2+json";
 
     @RequestMapping(
         method = RequestMethod.POST,
         value = "/refdata/judicial/users",
-        headers = CONTENT_TYPE + "=" + CONTENT_TYPE_V2)
+        consumes = CONTENT_TYPE_VAL,
+        headers = ACCEPT + "=" + CONTENT_TYPE_V2)
     List<JudgeDetail> getJudgeDetails(
         @RequestHeader(AUTHORIZATION) String authorization,
         @RequestHeader(SERVICE_AUTHORIZATION) String serviceAuthorization,
