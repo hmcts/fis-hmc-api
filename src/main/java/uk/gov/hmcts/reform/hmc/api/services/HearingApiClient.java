ddf86809bad350e5f3fb721aa2a9ffd904b53802
vasthav-patta <112877261+vasthav-patta@users.noreply.github.com>
2024-03-19 13:57:29 +0000

    test cases

--- src/main/java/uk/gov/hmcts/reform/hmc/api/services/HearingApiClient.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/api/services/HearingApiClient.java
@@ -4,8 +4,11 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.cloud.openfeign.FeignClientProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
+import uk.gov.hmcts.reform.hmc.api.model.request.HearingRequest;
 import uk.gov.hmcts.reform.hmc.api.model.response.Hearings;
 
 import java.util.List;
@@ -28,5 +31,12 @@ public interface HearingApiClient {
             @RequestHeader("Authorization") String authorisation,
             @RequestHeader("ServiceAuthorization") String serviceAuthorization,
             @RequestParam("ccdCaseRefs") List<String> ccdCaseRefs);
+
+
+    @PostMapping(path = "/hearings")
+    List<Hearings> createHearingDetails(
+            @RequestHeader("Authorization") String authorisation,
+            @RequestHeader("ServiceAuthorization") String serviceAuthorization,
+            @RequestBody HearingRequest hearingRequest);
 
 }
