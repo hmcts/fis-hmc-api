f50d689523f7209086a0fb2cb3378aad25db55b2
velmurugan-mani <velmurugan.mani@hmcts.net>
2024-03-20 17:04:10 +0000

    HearingTransactionRequestMapper Update

--- src/main/java/uk/gov/hmcts/reform/hmc/api/mapper/HearingTransactionRequestMapper.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/api/mapper/HearingTransactionRequestMapper.java
@@ -2,15 +2,40 @@ package uk.gov.hmcts.reform.hmc.api.mapper;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.reform.hmc.api.model.request.CaseCategories;
 import uk.gov.hmcts.reform.hmc.api.model.request.HearingRequest;
+
+import java.time.LocalDateTime;
+
+import static uk.gov.hmcts.reform.hmc.api.utils.Constants.CASE_FILE_VIEW;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class HearingTransactionRequestMapper {
+    @Value("${ccd.ui.url}")
+    private String ccdBaseUrl;
+
     public HearingRequest mappingHearingTransactionRequest(CaseDetails caseDetails) {
-        return new HearingRequest();
+        HearingRequest hearingRequest = new HearingRequest();
+        hearingRequest.setCaseDetails(uk.gov.hmcts.reform.hmc.api.model.request.CaseDetails.hearingWith()
+                                          .hmctsServiceCode("ABA5") //Hardcoded in prl-cos-api
+                                          .caseRef(caseDetails.getId().toString())
+                                          .requestTimeStamp(LocalDateTime.now())
+                                          .externalCaseReference("") //Need to verify
+                                          .caseDeepLink(ccdBaseUrl + "caseReference" + CASE_FILE_VIEW) //Need to verify
+                                          .hmctsInternalCaseName("")
+                                          .publicCaseName("")
+                                          .caseAdditionalSecurityFlag(Boolean.TRUE)
+                                          .caseInterpreterRequiredFlag(Boolean.TRUE)
+                                          .caseCategories(CaseCategories.CaseCategoriesWith().build())
+                                          .caseManagementLocationCode("")
+                                          .caseRestrictedFlag(Boolean.TRUE)
+                                          .caseSlaStartDate("")
+                                          .build());
+        return hearingRequest;
     }
 }
--- src/main/java/uk/gov/hmcts/reform/hmc/api/model/request/CaseDetails.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/api/model/request/CaseDetails.java
@@ -2,15 +2,20 @@ package uk.gov.hmcts.reform.hmc.api.model.request;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.joda.time.DateTime;
+
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Builder(builderMethodName = "hearingWith")
 public class CaseDetails {
 
     @JsonProperty("hmctsServiceCode")
@@ -20,7 +25,8 @@ public class CaseDetails {
     private String caseRef;
 
     @JsonProperty("requestTimeStamp")
-    private DateTime requestTimeStamp;
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    private LocalDateTime requestTimeStamp;
 
     @JsonProperty("externalCaseReference")
     private String externalCaseReference;
