bf76cc74235e8a9e0bb8865431b29409540592b3
Sahitya Desireddy <sahitya.desireddy@hmcts.net>
2024-04-11 16:03:56 +0100

    remove unused import

--- src/test/java/uk/gov/hmcts/reform/hmc/api/services/AutomateHearingServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/hmc/api/services/AutomateHearingServiceTest.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.reform.hmc.api.services;
 import org.json.simple.parser.ParseException;
 import org.junit.Ignore;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
