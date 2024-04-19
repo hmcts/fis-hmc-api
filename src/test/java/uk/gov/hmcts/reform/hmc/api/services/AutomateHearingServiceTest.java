0e611d19e049ce19cd07835ccb1371e028f005a9
hmcts <prudhvi.maddineni@hmcts.net>
2024-04-19 11:58:53 +0100

    chechstyle changes

--- src/test/java/uk/gov/hmcts/reform/hmc/api/services/AutomateHearingServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/hmc/api/services/AutomateHearingServiceTest.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.hmc.api.services;
 
 import org.json.simple.parser.ParseException;
-import org.junit.Ignore;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
