f5d0d910fafbfc5d23379608cdf4b6ecc4964aa8
Priyanka Singh <priyanka.singh@hmcts.net>
2024-05-14 17:10:15 +0100

    FPET-967: fixed checkstyle issue

--- src/test/java/uk/gov/hmcts/reform/hmc/api/controllers/RootControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/hmc/api/controllers/RootControllerTest.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.hmc.api.controllers;
 
-import org.junit.Before;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -19,13 +18,13 @@ public class RootControllerTest {
     @InjectMocks private RootController rootController;
 
     @BeforeEach
-    void setUp(){
+    void setUp() {
         MockitoAnnotations.openMocks(this);
 
     }
 
     @Test
-    void welcomeTest(){
+    void welcomeTest() {
         ResponseEntity<String> response = rootController.welcome();
         Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
     }
