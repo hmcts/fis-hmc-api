3791ad31f32a4b07d6be750a0e09fdf3931de707
Priyanka Singh <priyanka.singh@hmcts.net>
2024-05-16 16:00:57 +0100

    FPET-967: fixed checkstyle issue

--- src/main/java/uk/gov/hmcts/reform/hmc/api/controllers/HearingsController.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/api/controllers/HearingsController.java
@@ -18,7 +18,6 @@ import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.json.simple.parser.ParseException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
