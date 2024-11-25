e95bd593cd1eae28c372c8180fbdbe414dacc93a
bharadwajsairam <bharadwajsairam001@gmail.com>
2024-11-25 18:13:31 +0000

    checkstyle fix

--- src/main/java/uk/gov/hmcts/reform/hmc/api/services/HearingsServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/api/services/HearingsServiceImpl.java
@@ -180,7 +180,7 @@ public class HearingsServiceImpl implements HearingsService {
                             userToken, s2sToken, new ArrayList<>(caseIdWithRegionIdMap.keySet()));
             log.info("****List of hearing dertails** {}", hearingDetailsList);
             log.info("****List of hearing dertails size** {}", hearingDetailsList.size());
-            if(!CollectionUtils.isEmpty(hearingDetailsList)){
+            if (!CollectionUtils.isEmpty(hearingDetailsList)) {
                 for (var hearing : hearingDetailsList) {
                     try {
                         hearingDetails = hearing;
