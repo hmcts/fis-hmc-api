9176de1fabdbe0bac59f01b230186723ea07b892
Daniel Catchpole <daniel.catchpole@justice.gov.uk>
2025-10-10 16:22:22 +0100

    Move to new version of azure servicebus - see hmcts/civil-service#6397 for context

--- src/main/java/uk/gov/hmcts/reform/hmc/api/config/ServiceBusConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/api/config/ServiceBusConfiguration.java
@@ -1,15 +1,12 @@
 package uk.gov.hmcts.reform.hmc.api.config;
 
+import com.azure.messaging.servicebus.ServiceBusClientBuilder;
+import com.azure.messaging.servicebus.ServiceBusErrorContext;
+import com.azure.messaging.servicebus.ServiceBusProcessorClient;
+import com.azure.messaging.servicebus.ServiceBusReceivedMessage;
+import com.azure.messaging.servicebus.ServiceBusReceivedMessageContext;
+import com.azure.messaging.servicebus.models.ServiceBusReceiveMode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.microsoft.azure.servicebus.ExceptionPhase;
-import com.microsoft.azure.servicebus.IMessage;
-import com.microsoft.azure.servicebus.IMessageHandler;
-import com.microsoft.azure.servicebus.MessageHandlerOptions;
-import com.microsoft.azure.servicebus.ReceiveMode;
-import com.microsoft.azure.servicebus.SubscriptionClient;
-import com.microsoft.azure.servicebus.primitives.ConnectionStringBuilder;
-import com.microsoft.azure.servicebus.primitives.ServiceBusException;
-import lombok.SneakyThrows;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,11 +29,7 @@ import uk.gov.hmcts.reform.hmc.api.services.RefDataService;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.time.Duration;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 import static uk.gov.hmcts.reform.hmc.api.utils.Constants.ADJOURNED;
 import static uk.gov.hmcts.reform.hmc.api.utils.Constants.CANCELLED;
@@ -79,148 +72,136 @@ public class ServiceBusConfiguration {
     @Autowired AuthTokenGenerator authTokenGenerator;
 
     private static Logger log = LoggerFactory.getLogger(ServiceBusConfiguration.class);
+    private ServiceBusProcessorClient processorClient;
 
     @Bean
     @Profile("!test")
-    public SubscriptionClient receiveClient()
-            throws URISyntaxException, ServiceBusException, InterruptedException {
-        log.debug(" host {}", host);
-        log.debug(" sharedAccessKeyName {}", sharedAccessKeyName);
-        log.debug(" topic {}", topic);
-        log.debug(" sharedAccessKeyValue {}", sharedAccessKeyValue);
-        log.debug(" subscription {}", subscription);
+    public ServiceBusProcessorClient serviceBusProcessorClient() throws URISyntaxException{
         URI endpoint = new URI("sb://" + host);
+        String connectionString = "Endpoint=" + endpoint + "/"
+            + ";SharedAccessKeyName=" + sharedAccessKeyName
+            + ";SharedAccessKey=" + sharedAccessKeyValue;
 
-        String destination = topic.concat("/subscriptions/").concat(subscription);
+        processorClient = new ServiceBusClientBuilder()
+            .connectionString(connectionString)
+            .processor()
+            .topicName(topic)
+            .subscriptionName(subscription)
+            .receiveMode(ServiceBusReceiveMode.PEEK_LOCK)
+            .processMessage(this::processMessage)
+            .processError(this::processError)
+            .buildProcessorClient();
 
-        ConnectionStringBuilder connectionStringBuilder =
-                new ConnectionStringBuilder(
-                        endpoint, destination, sharedAccessKeyName, sharedAccessKeyValue);
-        connectionStringBuilder.setOperationTimeout(Duration.ofMinutes(10));
-        return new SubscriptionClient(connectionStringBuilder, ReceiveMode.PEEKLOCK);
+        processorClient.start();
+        log.info("HMC ServiceBusProcessorClient started successfully.");
+
+        return processorClient;
     }
 
-    @Bean
-    @Profile("!test")
-    CompletableFuture<Void> registerMessageHandlerOnClient(
-            @Autowired SubscriptionClient receiveClient)
-            throws ServiceBusException, InterruptedException {
+    private void processMessage(ServiceBusReceivedMessageContext context) {
+        try {
+            ServiceBusReceivedMessage message = context.getMessage();
 
-        IMessageHandler messageHandler =
-                new IMessageHandler() {
+            byte[] body = message.getBody().toBytes();
+            ObjectMapper mapper = new ObjectMapper();
 
-                    @SneakyThrows
-                    @Override
-                    public CompletableFuture<Void> onMessageAsync(IMessage message) {
-                        List<byte[]> body = message.getMessageBody().getBinaryData();
-                        ObjectMapper mapper = new ObjectMapper();
+            Hearing hearing = mapper.readValue(body, Hearing.class);
 
-                        Hearing hearing = mapper.readValue(body.get(0), Hearing.class);
+            if (HMCTS_SERVICE_ID.equals(hearing.getHmctsServiceCode())
+                && isHearingStateConsumptionRequired(hearing.getHearingUpdate().getHmcStatus())) {
+                HearingUpdateDTO hearingUpdateDto =
+                    HearingUpdateDTO.hearingUpdateRequestDTOWith()
+                        .hearingResponseReceivedDateTime(
+                            hearing.getHearingUpdate()
+                                .getHearingResponseReceivedDateTime())
+                        .hearingEventBroadcastDateTime(
+                            hearing.getHearingUpdate()
+                                .getHearingEventBroadcastDateTime())
+                        .hearingListingStatus(
+                            hearing.getHearingUpdate()
+                                .getHearingListingStatus())
+                        .nextHearingDate(
+                            hearing.getHearingUpdate().getNextHearingDate())
+                        .hearingVenueId(
+                            hearing.getHearingUpdate().getHearingVenueId())
+                        .hearingJudgeId(
+                            hearing.getHearingUpdate().getHearingJudgeId())
+                        .hearingRoomId(
+                            hearing.getHearingUpdate().getHearingRoomId())
+                        .hmcStatus(hearing.getHearingUpdate().getHmcStatus())
+                        .listAssistCaseStatus(
+                            hearing.getHearingUpdate()
+                                .getListAssistCaseStatus())
+                        .build();
 
-                        if (HMCTS_SERVICE_ID.equals(hearing.getHmctsServiceCode())
-                            && isHearingStateConsumptionRequired(hearing.getHearingUpdate().getHmcStatus())) {
-                            HearingUpdateDTO hearingUpdateDto =
-                                HearingUpdateDTO.hearingUpdateRequestDTOWith()
-                                    .hearingResponseReceivedDateTime(
-                                        hearing.getHearingUpdate()
-                                            .getHearingResponseReceivedDateTime())
-                                    .hearingEventBroadcastDateTime(
-                                        hearing.getHearingUpdate()
-                                            .getHearingEventBroadcastDateTime())
-                                    .hearingListingStatus(
-                                        hearing.getHearingUpdate()
-                                                            .getHearingListingStatus())
-                                            .nextHearingDate(
-                                                    hearing.getHearingUpdate().getNextHearingDate())
-                                            .hearingVenueId(
-                                                    hearing.getHearingUpdate().getHearingVenueId())
-                                            .hearingJudgeId(
-                                                    hearing.getHearingUpdate().getHearingJudgeId())
-                                            .hearingRoomId(
-                                                    hearing.getHearingUpdate().getHearingRoomId())
-                                            .hmcStatus(hearing.getHearingUpdate().getHmcStatus())
-                                            .listAssistCaseStatus(
-                                                    hearing.getHearingUpdate()
-                                                            .getListAssistCaseStatus())
-                                            .build();
+                HearingDTO hearingDto =
+                    HearingDTO.hearingRequestDTOWith()
+                        .hmctsServiceCode(hearing.getHmctsServiceCode())
+                        .caseRef(hearing.getCaseRef())
+                        .hearingId(hearing.getHearingID())
+                        .hearingUpdate(hearingUpdateDto)
+                        .build();
 
-                            HearingDTO hearingDto =
-                                    HearingDTO.hearingRequestDTOWith()
-                                            .hmctsServiceCode(hearing.getHmctsServiceCode())
-                                            .caseRef(hearing.getCaseRef())
-                                            .hearingId(hearing.getHearingID())
-                                            .hearingUpdate(hearingUpdateDto)
-                                            .build();
+                log.info("Service Bus message for PRL Update " + hearingDto);
 
-                            log.info("Service Bus message for PRL Update " + hearingDto);
+                if (hearingDto.getHearingUpdate().getHearingVenueId() != null
+                    && LISTED.equals(
+                    hearingDto.getHearingUpdate().getHmcStatus())) {
 
-                            if (hearingDto.getHearingUpdate().getHearingVenueId() != null
-                                    && LISTED.equals(
-                                            hearingDto.getHearingUpdate().getHmcStatus())) {
+                    log.info(
+                        "VenueId "
+                            + hearing.getHearingUpdate().getHearingVenueId());
+                    hearingDto = refDataService.getHearingWithCourtDetails(hearingDto);
+                    log.info(
+                        "Hearing with Full CourtDetails  "
+                            + hearingDto
+                            .getHearingUpdate()
+                            .getHearingVenueName());
+                }
+                String userToken = idamTokenGenerator.getSysUserToken();
+                String serviceToken = authTokenGenerator.generate();
 
-                                log.info(
-                                        "VenueId "
-                                                + hearing.getHearingUpdate().getHearingVenueId());
-                                hearingDto = refDataService.getHearingWithCourtDetails(hearingDto);
-                                log.info(
-                                        "Hearing with Full CourtDetails  "
-                                                + hearingDto
-                                                        .getHearingUpdate()
-                                                        .getHearingVenueName());
-                            }
-                            String userToken = idamTokenGenerator.getSysUserToken();
-                            String serviceToken = authTokenGenerator.generate();
+                Hearings hearings =
+                    hearingsService.getHearingsByCaseRefNo(
+                        hearingDto.getCaseRef(), userToken, serviceToken);
+                State caseState = null;
+                if (hearings != null) {
+                    NextHearingDetails nextHearingDetails =
+                        nextHearingDetailsService.getNextHearingDate(hearings);
+                    if (nextHearingDetails != null) {
+                        log.info("Next Hearing details " + nextHearingDetails);
+                        NextHearingDetailsDTO nextHearingDetailsDTO =
+                            NextHearingDetailsDTO.nextHearingDetailsRequestDTOWith()
+                                .nextHearingDetails(nextHearingDetails)
+                                .caseRef(hearings.getCaseRef())
+                                .build();
 
-                            Hearings hearings =
-                                    hearingsService.getHearingsByCaseRefNo(
-                                            hearingDto.getCaseRef(), userToken, serviceToken);
-                            State caseState = null;
-                            if (hearings != null) {
-                                NextHearingDetails nextHearingDetails =
-                                        nextHearingDetailsService.getNextHearingDate(hearings);
-                                if (nextHearingDetails != null) {
-                                    log.info("Next Hearing details " + nextHearingDetails);
-                                    NextHearingDetailsDTO nextHearingDetailsDTO =
-                                            NextHearingDetailsDTO.nextHearingDetailsRequestDTOWith()
-                                                    .nextHearingDetails(nextHearingDetails)
-                                                    .caseRef(hearings.getCaseRef())
-                                                    .build();
-
-                                    hearingDto.setNextHearingDateRequest(nextHearingDetailsDTO);
-                                }
-                                caseState =
-                                        nextHearingDetailsService.fetchStateForUpdate(
-                                                hearings,
-                                                hearingDto.getHearingUpdate().getHmcStatus());
-                                Boolean isPrlSuccess = false;
-                                isPrlSuccess =
-                                        prlUpdateService.updatePrlServiceWithHearing(
-                                                hearingDto, caseState);
-                                if (isPrlSuccess) {
-                                    return receiveClient.completeAsync(message.getLockToken());
-                                }
-                            }
-                            return receiveClient.abandonAsync(message.getLockToken());
-                        }
-                        return receiveClient.completeAsync(message.getLockToken());
+                        hearingDto.setNextHearingDateRequest(nextHearingDetailsDTO);
                     }
-
-                    @Override
-                    public void notifyException(
-                            Throwable throwable, ExceptionPhase exceptionPhase) {
-                        log.error("Exception occurred.");
-                        log.error(exceptionPhase + "-" + throwable.getMessage());
+                    caseState =
+                        nextHearingDetailsService.fetchStateForUpdate(
+                            hearings,
+                            hearingDto.getHearingUpdate().getHmcStatus());
+                    Boolean isPrlSuccess = false;
+                    isPrlSuccess =
+                        prlUpdateService.updatePrlServiceWithHearing(
+                            hearingDto, caseState);
+                    if (isPrlSuccess) {
+                        context.complete();
                     }
-                };
+                }
+                context.abandon();
+            }
+            context.complete();
+        } catch (Exception e) {
+            log.error("There was a problem processing the message: {}", e.getMessage(), e);
+            context.abandon();
+        }
+    }
 
-        ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
-        receiveClient.registerMessageHandler(
-            messageHandler,
-            new MessageHandlerOptions(
-                threadCount, false, Duration.ofHours(1), Duration.ofMinutes(5)),
-            executorService
-        );
-        return null;
+    private void processError(ServiceBusErrorContext context) {
+        log.error("Exception occurred while processing message");
+        log.error("{} - {}", context.getErrorSource(), context.getException().getMessage(), context.getException());
     }
 
     private boolean isHearingStateConsumptionRequired(String hearingStatus) {
