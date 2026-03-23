c5ec2f49a365deb0e518fe7055ceddd7ef85cfe5
James Turnbull <james.turnbull@justice.gov.uk>
2026-03-23 08:09:28 +0000

    Fix checkstyle error

--- src/test/java/uk/gov/hmcts/reform/hmc/api/controllers/HearingControllerNextHearingDetailsTest.java
+++ src/test/java/uk/gov/hmcts/reform/hmc/api/controllers/HearingControllerNextHearingDetailsTest.java
@@ -4,7 +4,6 @@ import feign.FeignException;
 import feign.Request;
 import feign.Response;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
